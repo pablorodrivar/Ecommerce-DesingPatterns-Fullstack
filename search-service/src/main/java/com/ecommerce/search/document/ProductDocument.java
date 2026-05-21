0bfb126ef3b941c8072db45eb625bf2fb273a538
Pablo Rodriguez Vargas <pablorodriguez12150@gmail.com>
2026-05-21 13:59:09 +0200

    fix: correct Elasticsearch date format for LocalDateTime in ProductDocument

--- search-service/src/main/java/com/ecommerce/search/document/ProductDocument.java
+++ search-service/src/main/java/com/ecommerce/search/document/ProductDocument.java
@@ -2,6 +2,7 @@ package com.ecommerce.search.document;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
@@ -37,6 +38,6 @@ public class ProductDocument {
     @Field(type = FieldType.Keyword)
     private String status;
 
-    @Field(type = FieldType.Date)
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
     private LocalDateTime createdAt;
 }
